b6a9e89ca2eb660b22c9f71132383d50e54c7909
udeba <143026150+udeba@users.noreply.github.com>
2023-12-08 10:40:34 +0100

    person data created

---
+++ script.js
@@ -0,0 +1,33 @@
+const studentData = [
+  { name: "Max Hermann", role: "Fullstack Developer", grade: 5.4 },
+  { name: "Anthony Egbe", role: "Cloud Specialist", grade: 4.9 },
+];
+
+const teachersData = [
+  {
+    name: "Hannes Buhler",
+    role: "Javascript Expert",
+    linkStudent: "studentsLink",
+    linkClass: "classLink",
+  },
+  {
+    name: "Ali SAYAR",
+    role: "AWS Expert",
+    linkStudent: "studentsLink",
+    linkClass: "classLink",
+  },
+];
+const classesData = [
+  {
+    name: "Javascript Expert",
+    teacherName: "Hannes Buhler",
+    linkStudent: "studentsLink",
+    linkTeacher: "teacherLink",
+  },
+  {
+    name: "AWS Expert",
+    teacherName: "Ali SAYAR",
+    linkStudent: "studentsLink",
+    linkTeacher: "teacherLink",
+  },
+];
